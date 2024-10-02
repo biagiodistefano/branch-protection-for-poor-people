55d38f46c8926aecf3f05987060ef2209ce3f073
Biagio Distefano <biagiodistefano92@gmail.com>
2024-10-02 23:40:29 +0200

    add local install script

---
+++ install_global_hooks.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Set up the global Git hooks template directory
+TEMPLATE_DIR="$HOME/.git-templates/hooks"
+HOOK_URL="https://raw.githubusercontent.com/biagiodistefano/branch-protection-for-poor-people/main/pre-push"
+
+# Create the hooks template directory if it doesn't exist
+mkdir -p "$TEMPLATE_DIR"
+
+# Download the pre-push hook script from your GitHub repository
+curl -fsSL -o "$TEMPLATE_DIR/pre-push" "$HOOK_URL"
+
+# Make the pre-push script executable
+chmod +x "$TEMPLATE_DIR/pre-push"
+
+# Set Git to use the hooks template directory system-wide
+git config --global init.templateDir "$HOME/.git-templates"
+
+# Output a success message
+echo "Global pre-push hook installed. All *future* repositories will have this hook."
